a246024f12fddb2abd3cbf7d6e855fa4e24b0f43
varekai <support@x-bet.co>
2020-07-01 23:07:27 +0300

    event work

--- src/components/app-header.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const AppHeader = () => {
-  return <h1>My todo list</h1>;
-};
-
-export default AppHeader;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
-import ToDoList from './components/todo-list';
-
-const App = () => {
-  const value = '<script>alert("howdy")</script>';
-  return (
-    <div>
-      {value}
-      <AppHeader />
-      <SearchPanel />
-      <ToDoList />
-    </div>
-  );
-};
+import App from './components/app';
 
 ReactDOM.render(<App />, document.getElementById('root'));
